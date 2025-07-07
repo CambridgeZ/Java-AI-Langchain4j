077f2d7e1068b210abb08b735ce67a0dec2a3936
CambridgeZ <1376734195@qq.com>
2025-07-07 23:22:16 +0800

    init something for java Ai xiaozhi

---
+++ src/test/java/org/jianqiaozh/java/ai/langchain4j/LLMTest.java
@@ -0,0 +1,21 @@
+package org.jianqiaozh.java.ai.langchai4j;
+
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LLMTest {
+
+    @Test
+    public void testGPTDemo(){
+        OpenAiChatModel model = OpenAiChatModel.builder()
+                .baseUrl("http://langchain4j.dev/demo/openai/v1")
+                .apiKey("demo")
+                .modelName("gpt-4o-mini")
+                .build();
+
+       String answer =  model.chat("你好");
+       System.out.println("Answer: " + answer);
+    }
+}
