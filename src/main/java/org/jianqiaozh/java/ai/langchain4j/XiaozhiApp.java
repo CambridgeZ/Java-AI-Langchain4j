1607c515f05978d6d1377d7a63f499856dea750b
CambridgeZ <1376734195@qq.com>
2025-07-07 23:22:33 +0800

    init

--- src/main/java/org/jianqiaozh/java/ai/langchain4j/XiaozhiApp.java
+++ src/main/java/org/jianqiaozh/java/ai/langchain4j/XiaozhiApp.java
@@ -1,7 +1,11 @@
-package org.jianqiaozh;
+package org.jianqiaozh.java.ai.langchain4j;
 
-public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaozhiApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SpringApplication.run(XiaozhiApp.class, args);
     }
 }
--- src/test/java/org/jianqiaozh/java/ai/langchain4j/LLMTest.java
+++ src/test/java/org/jianqiaozh/java/ai/langchain4j/LLMTest.java
@@ -1,7 +1,8 @@
-package org.jianqiaozh.java.ai.langchai4j;
+package org.jianqiaozh.java.ai.langchain4j;
 
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -17,5 +18,17 @@ public class LLMTest {
 
        String answer =  model.chat("你好");
        System.out.println("Answer: " + answer);
+    }
+
+    @Autowired
+    private OpenAiChatModel openAiChatModel;
+
+    public static final String BLUE = "\u001B[34m";
+    public static final String RESET = "\u001B[0m";
+
+    @Test
+    public void testSpringBoot(){
+        String ans = openAiChatModel.chat("我是谁？");
+        System.out.println(BLUE + ans + RESET);
     }
 }
