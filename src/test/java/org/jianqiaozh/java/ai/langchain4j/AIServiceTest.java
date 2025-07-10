dc37eee73cc6dc1411996c42f4670f67b114fdf7
CambridgeZ <1376734195@qq.com>
2025-07-10 22:21:24 +0800

    add AIService

--- src/test/java/org/jianqiaozh/java/ai/langchain4j/AIServiceTest.java
+++ src/test/java/org/jianqiaozh/java/ai/langchain4j/AIServiceTest.java
@@ -1,4 +1,31 @@
 package org.jianqiaozh.java.ai.langchain4j;
 
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.service.AiServices;
+import org.jianqiaozh.java.ai.langchain4j.assistant.Assistant;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class AIServiceTest {
+
+    @Autowired
+    private QwenChatModel qwenChatModel;
+
+    @Test
+    public void testChat() {
+        Assistant assistant = AiServices.create(Assistant.class, qwenChatModel);
+        String answer = assistant.chat("你是谁");
+        System.out.println(answer);
+    }
+
+    @Autowired
+    private Assistant assistant;
+
+    @Test
+    public void testAssistant() {
+        String answer = assistant.chat("告诉我一些关于Java的知识");
+        System.out.println(answer);
+    }
 }
